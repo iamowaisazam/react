55adf25d83d8641044e6ceecdd1bc023e0d1e31a
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2025-04-15 02:50:50 -0500

    remove the zip file and also bootstrape min js file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import './index.css'
